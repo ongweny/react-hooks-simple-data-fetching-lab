7a5dae0d0cd0895ee6e75036bf2818d066b902ea
ongweny <ongwenyjoel@gmail.com>
2024-05-25 11:33:28 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App(){
+    const [pic, setPic]= useState("null");
+
+    useEffect(()=> {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res)=>res.json())
+        .then((data) => {
+            console.log(data)
+            setPic(data.message)
+        })
+        .catch((error) => {
+            console.error("Error fetching the dog image:", error);
+          });
+    },[])
+
+    if (pic === null){
+    return <p>Loading...</p> ;
+    }
+    return (<img src={pic} alt="A Random Dog"/>)
+}
+
+export default App;
